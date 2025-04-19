a6b0ef05e9f891ed4184d57082088fd4c4ee1041
Your Name <raxmatoveldor37@gmail.com>
2025-04-19 10:06:48 +0500

    operation and calculate function was created and worked one operation *

--- components/main/main.js
+++ components/main/main.js
@@ -4,30 +4,67 @@ import MainStyle from "./main.style";
 
 import Screen from "../screen/screen";
 import Buttons from "../buttons/buttons";
+import {Operations} from "../buttons/buttons.enum";
 
 export default function Main() {
     const [result, setResult] = useState(0);
     const [events, setEvents] = useState([0]);
 
-    const calculate = (events) => {
-        let {operations, numbers} = events.reduce((acc, event) => {
+    const regex_num = /^\d*\.?\d*$/;
+    const regex_ope = /^[+\-*/]$/;
 
-        }, {operations, numbers});
+    const operation = (first, second, oper) => {
+        console.log("operators: ", first, second, oper);
+        switch (oper) {
+            case "+":
+                first += second;
+                break;
+            case "-":
+                first -= second;
+                break;
+            case "*":
+                first *= second;
+                break;
+            case "/":
+                first /= second;
+                break;
+            default:
+                break;
+        }
+
+        return first;
+    }
+
+    const calculate = (events) => {
+        return events.reduce((acc, event) => {
+            if (event.includes(Operations[0])) {
+                let index = events.indexOf(Operations[0]);
+                const result = operation(+events[index - 1], +events[index + 1], event);
+                console.log("x: ", result);
+                events = [...events.slice(0, index - 1), result, events.slice(index + 2)];
+            }
+            return events;
+        });
     }
 
     const handleButtonsUpdate = (new_value) => {
-        const regex = /^(\d*\.?\d*$|[+\-*/])$/;
-        if (regex.test(new_value)) {
+        if (regex_num.test(new_value)) {
             if (new_value === "." && events.includes(new_value)) return;
             if (!events[0] && events.length === 1 && new_value !== ".") {
                 if (new_value === "0") return;
 
                 events[0] = new_value;
                 setResult(new_value);
-            } else {
-                if (events.length <= 9) {
-                    events.push(new_value);
-                }
+            } else if (events.length <= 9) {
+                events.push(new_value);
+                setResult(events.join(''));
+            }
+        } else if (regex_ope.test(new_value)) {
+            if (Operations.includes(events[events.length - 1])) {
+                events[events.length - 1] = new_value;
+                setResult(events.join(''));
+            } else if (events[0]) {
+                events.push(new_value);
                 setResult(events.join(''));
             }
         } else {
@@ -46,7 +83,8 @@ export default function Main() {
                     }
                     break;
                 case "=":
-                // calculate all operation by priority...
+                    // calculate all operation by priority...
+                    setResult(calculate(events).join(''));
             }
         }
     }
